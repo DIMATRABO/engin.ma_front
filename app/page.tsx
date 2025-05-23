8f1d2f1362ee48dbb58b4865f9f194e94ee45cc8
user <dimatrabo007@gmail.com>
2025-05-23 22:35:02 +0100

    hello word

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-white text-black">
+      <h1 className="text-4xl font-bold">Hello from ENGİN.MA 🚜</h1>
+    </main>
+  );
+}
