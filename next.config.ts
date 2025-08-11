b8bc7810b2adf8ad143dad13332b2bb72d7f4804
Trabo007 <107281343+DIMATRABO@users.noreply.github.com>
2025-08-11 01:21:07 +0100

    Disable ESLint

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Disable ESLint checks during build
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
